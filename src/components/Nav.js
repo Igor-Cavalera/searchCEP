d095e2d5468f4cf015e6c3cf299b3ed93867a68f
Igor Cavalera <66530386+Igor-Cavalera@users.noreply.github.com>
2022-04-30 19:18:33 -0300

    adicionando-componente-tag-nav

---
+++ src/components/Nav.js
@@ -0,0 +1,17 @@
+function Nav () {
+    return (
+        <>    
+            <nav>
+                <div class="left-nav">
+                    <span class="nav-link">&#x2630;</span>
+                </div>
+                <div class="right-nav">
+                    <span class="nav-link" id="minimize">&minus;</span>
+                    <span class="nav-link" id="close-app">&#10006;</span>
+                </div>
+            </nav>
+        </>
+    )
+}
+
+export default Nav;
